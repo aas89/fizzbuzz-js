2564e9ce8752f94a94a7281ace01a302f31d843a
Anya Sims <Anya.Sims@softwire.com>
2019-12-09 15:43:46 +0000

    implement the rest of the rules for 11, 13, 17 (part 2)

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,19 +6,44 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-    var i;
-    for (i = 0; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0){
-            console.log("FizzBuzz");
-        } else if (i % 3 == 0){
-            //break
-            console.log('Fizz');
-        } else if (i % 5 == 0){
-            //break
-            console.log('Buzz');
-            //break
+    for (let i = 1; i <= 100; i++) {
+        let print_out = "";
+
+        if (i % 3 == 0){
+            print_out += 'Fizz';
+
+        } if (i % 13 == 0){            
+            print_out += 'Fezz';
+            
+        } if (i % 5 == 0){            
+            print_out += 'Buzz';
+            
+        } if (i % 7 == 0){            
+            print_out += 'Bang';
+            
+        } if (i % 11 == 0){
+            if (i % 13 == 0){
+                print_out = 'FezzBong'
+            } else {
+                print_out = 'Bong';
+            }
+            
+        } if (i % 17 == 0){
+            print_out += " "
+            new_print_out = ""
+            for (let n = 1; n <= (print_out.length / 4); n++) {
+                new_print_out += print_out.slice(-4*n-1, -4*(n-1)-1);
+            }          
+            print_out = new_print_out;
+            
         } else {
-            console.log(i);
+            ;
+        }
+
+        if (print_out != "") {
+            console.log(print_out);
+        } else {
+        console.log(i);
         }
 
     }
